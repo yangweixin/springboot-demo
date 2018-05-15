04596d517564f7e6d68ebbe0ded2727cdc296fac
yangweixin <yangweixin@codemao.cn>
2018-05-15 19:03:05 +0800

    feature(filter): add filter

--- src/main/java/top/oyoung/springbootdemo/configure/WebSecurityConfigurer.java
+++ src/main/java/top/oyoung/springbootdemo/configure/WebSecurityConfigurer.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
+import top.oyoung.springbootdemo.filter.AntiXXSFilter;
 import top.oyoung.springbootdemo.filter.LoginAuthenticationProvider;
 
 /**
@@ -20,9 +22,14 @@ public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private LoginAuthenticationProvider authenticationProvider;
+    @Autowired
+    private AntiXXSFilter antiXXSFilter;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
+        http.addFilterBefore(antiXXSFilter, FilterSecurityInterceptor.class);
+
         http
             .formLogin()
                 .and()
---
+++ src/main/java/top/oyoung/springbootdemo/filter/AntiXXSFilter.java
@@ -0,0 +1,25 @@
+package top.oyoung.springbootdemo.filter;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.logging.Filter;
+
+/**
+ * @Author: Yang Weixin
+ * @Description:
+ * @DateTime: 2018/5/15 下午6:52
+ */
+@Component
+public class AntiXXSFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+        System.out.printf("hello ~~ i am filter");
+    }
+}
